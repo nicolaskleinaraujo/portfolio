5b18e3c401c65e4b9cec45669dc9343761909ebe
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2023-11-06 23:03:21 -0300

    change language function added

--- src/funcs.js
+++ src/funcs.js
@@ -1,10 +1,28 @@
-const scrollBtn = document.getElementById('scrollBtn')
+/* Change Language Function */
+const brPage = document.getElementById('pt-br')
+const enPage = document.getElementById('en')
 var actualLang = 'pt-br'
-const pageHTML = window.document.head
 
-scrollBtn.addEventListener('click', () => {
-    window.scrollTo(0, 0)
-})
+function changeToPT() {
+    if (actualLang == 'en') {
+        actualLang = 'pt-br'
+        document.documentElement.lang = 'pt-br'
+        enPage.style.display = 'none'
+        brPage.style.display = 'block'
+    }
+}
+
+function changeToEN() {
+    if (actualLang == 'pt-br') {
+        actualLang = 'en'
+        document.documentElement.lang = 'en'
+        brPage.style.display = 'none'
+        enPage.style.display = 'block'
+    }
+}
+
+/* Hide Scroll Button Function */
+const scrollBtn = document.getElementById('scrollBtn')
 
 document.addEventListener('scroll', () => {
     if (window.scrollY > 200) {
@@ -13,10 +31,3 @@ document.addEventListener('scroll', () => {
         scrollBtn.style.display = "none"
     }
 })
-
-function changeLang() {
-    if (actualLang == 'pt-br') {
-        actualLang = 'en'
-        HTMLAllCollection
-    }
-}
