e8fb2231b473433ad485066006b3ddc183e81eec
nicolaskleinaraujo <141509949+nicolaskleinaraujo@users.noreply.github.com>
2023-11-13 19:50:35 -0300

    added a temporary function because my site is not hosted

--- src/scripts/funcs.js
+++ src/scripts/funcs.js
@@ -30,3 +30,12 @@ function showHelp() {
         window.alert('Run the ".exe" file in the same folder as the ".csv" file.')
     }
 }
+
+// Temporary function (404 page)
+function notHosted() {
+    if (actualLang == 'pt-br') {
+        window.alert('Infelizmente, este site não está hospedado no momento. Você pode acessar o meu repositório através do link ao lado e instalá-lo localmente. Caso não saiba como fazer isso, o README possui um tutorial.')
+    } else {
+        window.alert('Unfortunately this site is not currently hosted, you can access my repository via the link below and install it locally. If you dont know how to do this, the README has a tutorial.')
+    }
+}
